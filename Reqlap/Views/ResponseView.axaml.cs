92614cc44f190276dc6ee0dba9d24dc18d25b5b6
laser42 <vadimracu@list.ru>
2024-02-15 10:29:14 +0300

    ResponseView

--- Reqlap/Views/ResponseView.axaml.cs
+++ Reqlap/Views/ResponseView.axaml.cs
@@ -4,9 +4,12 @@ namespace Reqlap.Views
 {
     public partial class ResponseView : UserControl
     {
+        private readonly TextBox _requestBodyTextBox;
         public ResponseView()
         {
             InitializeComponent();
+
+            _requestBodyTextBox = this.FindControl<TextBox>("RequestBodyTextBox")!;
         }
     }
 }
